8831a6b327e3a022012ca84adea1284ec853983e
K.A <akkazu1121@gmail.com>
2025-12-11 19:16:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -58,14 +58,7 @@ app.post('/posts/:postId/comments', async (req,res)=>{
   await db.write();
   res.json({ success:true });
 });
-// -------- 管理画面（パスワード保護） --------
-app.get('/admin', (req, res) => {
-  const pass = req.query.pass;
-  if (pass !== 'kazuma1121') {
-    return res.status(401).send('認証エラー: パスワードが違うよ');
-  }
-  res.sendFile(__dirname + '/public/admin.html');
-});
+
 
 // -------------------- 管理者ログイン・ユーザー管理 --------------------
 
@@ -235,6 +228,15 @@ app.post('/kanri/delete-comment/:postId/:commentId', async (req,res)=>{
     await db.write();
   }
   res.send('ok');
+});
+
+// -------- 管理画面（パスワード保護） --------
+app.get('/admin', (req, res) => {
+  const pass = req.query.pass;
+  if (pass !== 'kazuma1121') {
+    return res.status(401).send('認証エラー: パスワードが違うよ');
+  }
+  res.sendFile(__dirname + '/public/admin.html');
 });
 
 // -------------------- 管理画面 HTML --------------------
